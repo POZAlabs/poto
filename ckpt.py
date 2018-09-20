68ffde8f9de5e9cf5dd0f59bbd8a0d3a1e904625
koomook <bongbonggg97@gmail.com>
2018-09-20 18:27:30 +0900

    Add ckpt downloading

---
+++ ckpt.py
@@ -0,0 +1,41 @@
+import numpy as np
+import os
+import re
+from .object import list_object_specific, download_file
+
+MODEL_FILENAME = 'model_ckpt'
+CKPT_START = MODEL_FILENAME+'-'
+CKPT_EXTENSIONS = ['data-00000-of-00001', 'index', 'meta']
+
+
+def download_ckpt(s3, ckpt_dir, training=False):
+    """Download latest ckpt from S3
+
+    Args:
+        s3: Verified s3 client object from boto
+        ckpt_dir: checkpoints dir that starts with `checkpoints`, ends with `/`
+        training: using this function as training mode or inference mode
+    
+    Returns:
+        None
+    """
+    if ckpt_dir[-1] != '/':
+        ckpt_dir += '/'
+    try:
+        objects = list_object_specific(s3, 'checkpoints', ckpt_dir)
+    except KeyError as e:
+        if training:
+            return None
+        else:
+            raise KeyError(e)
+    else:
+        latest_step = max(get_ckpt_steps(objects))
+        download_file(s3, 'checkpoints', os.path.join(ckpt_dir, 'checkpoint'))
+        for extension in CKPT_EXTENSIONS:
+            download_file(s3=s3,
+                        bucket_name='checkpoints',
+                        object_name=os.path.join(ckpt_dir, "{}-{}.{}".format(MODEL_FILENAME, latest_step, extension)))
+
+def get_ckpt_steps(objects):
+    return np.unique([int(obj.split(CKPT_START)[-1].split('.')[0])
+                      for obj in objects if re.search(CKPT_START, obj)])
