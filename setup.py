2e04160a9c38aa13fe11f796c6d2653de93c358a
koomook <bongbonggg97@gmail.com>
2018-10-04 18:21:23 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='poto',
+    version='0.2',
+    description='boto3 wrapper for deep learning',
+    url='https://github.com/pozalabs/poto',
+    author='Koomook',
+    author_email='contact@pozalabs.com',
+    license='Unlicense',
+    packages=['poto'],
+    install_requires=[
+        'boto3',
+        'numpy',
+    ],
+    # setup_requires=["pytest-runner"],
+    # tests_require=["pytest"],
+    # include_package_data=True,
+    zip_safe=False
+)
