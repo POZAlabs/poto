9c7677d477bbdd0961beac0330984bdca6e5e14a
koomook <bongbonggg97@gmail.com>
2018-10-05 17:39:54 +0900

    Fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='poto',
-    version='1.0.1',
+    version='1.0.2',
     description='boto3 wrapper for deep learning',
     url='https://github.com/pozalabs/poto',
     author='Koomook',
