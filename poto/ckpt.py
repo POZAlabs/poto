b076c8a00b89634c2385b2628c2a386322e67e2a
jam <gpalektzja@gmail.com>
2019-12-27 15:55:49 +0900

    fea) add download_model_config

--- poto/ckpt.py
+++ poto/ckpt.py
@@ -21,19 +21,7 @@ def download_ckpt(s3, ckpt_dir, training=False):
     Returns:
         None
     """
-    if ckpt_dir[-1] != '/':
-        ckpt_dir += '/'
-
-    splited = ckpt_dir.split('/')
-    start_idx = -1
-    for i, path in enumerate(splited):
-        if path == 'checkpoints':
-            start_idx = i
-
-    assert start_idx != -1 , f"{ckpt_dir} doesn't contain `checkpoints`"
-
-    prefix = "/".join(splited[start_idx:])
-
+    prefix = get_prefix(ckpt_dir)
     try:
         objects = list_object_specific(s3, 'checkpoints', prefix)
     except KeyError as e:
@@ -59,19 +47,49 @@ def download_ckpt(s3, ckpt_dir, training=False):
                         object_name=os.path.join(prefix, file_),
                         local_file_path=os.path.join(ckpt_dir, file_))
         
-        # prefix format: checkpoints/some/
-        project_name = os.path.basename(os.path.dirname(prefix))
-        config_file = CONFIG_FORMAT.format(project_name)
-        
-        try:
-            download_file(
-                s3=s3,
-                bucket_name='checkpoints',
-                object_name=os.path.join(prefix, config_file),
-                local_file_path=os.path.join(ckpt_dir, config_file)
-            )
-        except ClientError:
-            print(f"{project_name} doesn't have model config.json")
+
+def download_model_config(s3, ckpt_dir):
+    """Download model config
+
+    Args:
+        s3: Verified s3 client object from boto
+        ckpt_dir: checkpoints dir contains `checkpoints`
+    
+    Returns:
+        config path: str
+    """
+    prefix = get_prefix(ckpt_dir)
+    # prefix format: checkpoints/some/
+    project_name = os.path.basename(os.path.dirname(prefix))
+    config_file = CONFIG_FORMAT.format(project_name)
+    
+    try:
+        download_file(
+            s3=s3,
+            bucket_name='checkpoints',
+            object_name=os.path.join(prefix, config_file),
+            local_file_path=os.path.join(ckpt_dir, config_file)
+        )
+    except ClientError:
+        print(f"{project_name} doesn't have model config.json")
+
+    return os.path.join(ckpt_dir, config_file)
+
+
+def get_prefix(ckpt_dir):
+    if ckpt_dir[-1] != '/':
+        ckpt_dir += '/'
+
+    splited = ckpt_dir.split('/')
+    start_idx = -1
+    for i, path in enumerate(splited):
+        if path == 'checkpoints':
+            start_idx = i
+    assert start_idx != -1 , f"{ckpt_dir} doesn't contain `checkpoints`"
+    prefix = "/".join(splited[start_idx:])
+
+    return prefix
+
 
 def get_ckpt_steps(objects):
     return np.unique([int(obj.split(CKPT_START)[-1].split('.')[0])
